65a317d385572399d5d9db018467e249d05ccf18
Ahmed Fasih <wuzzyview@gmail.com>
2021-11-12 20:06:09 -0800

    db->ts

---
+++ db-v1.sql
@@ -0,0 +1,47 @@
+create table _yamanote_db_state (schemaVersion integer not null);
+insert into
+  _yamanote_db_state (schemaVersion)
+values
+  (1);
+create table user (
+  id INTEGER PRIMARY KEY,
+  name text unique not null,
+  hashed text not null,
+  salt text not null,
+  iterations integer not null,
+  keylen integer not null,
+  digest text not null
+);
+create table bookmark (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  userId integer not null,
+  url text not null,
+  title text not null,
+  createdTime float not null,
+  modifiedTime float not null,
+  render text not null,
+  renderedTime float not null,
+  unique (url, title)
+);
+create table comment (
+  id INTEGER PRIMARY KEY,
+  bookmarkId integer not null,
+  content text not null,
+  createdTime float not null,
+  modifiedTime float not null,
+  render text not null,
+  renderedTime float not null
+);
+create table backup (
+  id INTEGER PRIMARY KEY,
+  bookmarkId integer not null,
+  content text not null,
+  createdTime float not null
+);
+create table media (
+  id INTEGER PRIMARY KEY,
+  base64 text not null,
+  createdTime float not null,
+  checksumValue text not null,
+  checksumAlgo text not null
+);
