68dcab9a9d4d44a8a6bab74ea7864beb121a8d2f
Ahmed Fasih <wuzzyview@gmail.com>
2021-11-16 19:49:10 -0800

    bookmarklet working EVEN ON GITHUB via postMessage

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,10 +1,12 @@
-q = location.href;
-p = document.title;
-s = window.getSelection().toString();
-obj = {
-  url: document.location.href || '',
-  title: document.title || '',
-  comment: s,
-};
-fetch('http://localhost:3456/bookmark',
-      {method: 'POST', mode: 'cors', body: JSON.stringify(obj), headers: {'Content-Type': 'application/json'}})
+javascript: (function() {
+  q = location.href;
+  p = document.title;
+  s = window.getSelection().toString();
+  obj = {
+    url: document.location.href || '',
+    title: document.title || '',
+    comment: s,
+  };
+  t = window.open('http://localhost:3456', 'Yamanote', 'toolbar=no,width=100,height=100');
+  setTimeout(() => { t.postMessage(JSON.stringify(obj), 'http://localhost:3456'); }, 500);
+})()
